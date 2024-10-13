3611420c8470d345b720771497c3bb0c8b5e03f4
nehaldo <158427969+nehaldo@users.noreply.github.com>
2024-10-13 12:37:45 +0400

    include footer

--- index.php
+++ index.php
@@ -467,53 +467,7 @@
             </div>
         </section>
 
-        <footer class="section-p1">
-            <div class="col">
-                <img class="logo" src="img/ZNlogo-nobg.png" alt="">
-                <h4>Contact</h4>
-                <p><strong>Address: </strong>Reduit, Mauritius</p>
-                <p><strong>Phone: </strong>+230-5708-4622</p>
-                <p><strong>Hours: </strong>24/7 online service</p>
-                <div class="follow">
-                    <h4>Follow us</h4>
-                    <div class="icon">
-                        <i class="fab fa-facebook-f"></i>
-                        <i class="fab fa-twitter"></i>
-                        <i class="fab fa-instagram"></i>
-                        <i class="fab fa-pinterest-p"></i>
-                        <i class="fab fa-youtube"></i>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col"> 
-                <h4>About</h4>
-                <a href="#">About us</a>
-                <a href="#">Delivery Information</a>
-                <a href="#">Privacy Policy</a>
-                <a href="#">Terms & Conditions</a>
-                <a href="#">Contact Us</a>
-            </div>
-
-            <div class="col"> 
-                <h4>My Account</h4>
-                <a href="#">Sign In</a>
-                <a href="#">View Cart</a>
-                <a href="#">My wishlist</a>
-                <a href="#">Track my order</a>
-                <a href="#">Help</a>
-            </div>
-
-            <div class="payment">
-                <h4>Secured Payment Gateways</h4>
-                <img src="img/payment-removebg-preview.png" alt="">
-            </div>
-
-            <div class="copyright">
-                <p>© 2024 ZN Tech</p>
-            </div>
-
-        </footer>
+        <?php include 'footer.php'; ?>
         
         
         <script>
